deb8382d1baa7c5e9c0ec81086db489ab72c48bd
qtian13 <qiushuang.tian@gmail.com>
2021-08-25 01:15:13 -0700

    add project info

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,10 +1,80 @@
 import React from 'react';
 
+const project1 = {
+  id: "project1",
+  name: "CLOVE",
+  skills: "CSS/JavaScript/NodeJS/Express/Handlebars/Sequelize/Cloudinary/Heroku",
+  deployed: "https://still-reef-24172.herokuapp.com/",
+  github: "https://github.com/qtian13/CLOVE",
+  demo: "./assets/images/clove.png"
+};
+
+const project2 = {
+  id: "project2",
+  name: "CLOVE",
+  skills: "CSS/JavaScript/NodeJS/Express/Handlebars/Sequelize/Cloudinary/Heroku",
+  deployed: "https://still-reef-24172.herokuapp.com/",
+  github: "https://github.com/qtian13/CLOVE",
+  demo: "./assets/images/clove.png"
+};
+
+const project3 = {
+  id: "project3",
+  name: "CLOVE",
+  skills: "CSS/JavaScript/NodeJS/Express/Handlebars/Sequelize/Cloudinary/Heroku",
+  deployed: "https://still-reef-24172.herokuapp.com/",
+  github: "https://github.com/qtian13/CLOVE",
+  demo: "./assets/images/clove.png"
+};
+
+const project4 = {
+  id: "project4",
+  name: "CLOVE",
+  skills: "CSS/JavaScript/NodeJS/Express/Handlebars/Sequelize/Cloudinary/Heroku",
+  deployed: "https://still-reef-24172.herokuapp.com/",
+  github: "https://github.com/qtian13/CLOVE",
+  demo: "./assets/images/clove.png"
+};
+
+const project5 = {
+  id: "project5",
+  name: "CLOVE",
+  skills: "CSS/JavaScript/NodeJS/Express/Handlebars/Sequelize/Cloudinary/Heroku",
+  deployed: "https://still-reef-24172.herokuapp.com/",
+  github: "https://github.com/qtian13/CLOVE",
+  demo: "./assets/images/clove.png"
+};
+
+const project6 = {
+  id: "project6",
+  name: "CLOVE",
+  skills: "CSS/JavaScript/NodeJS/Express/Handlebars/Sequelize/Cloudinary/Heroku",
+  deployed: "https://still-reef-24172.herokuapp.com/",
+  github: "https://github.com/qtian13/CLOVE",
+  demo: "./assets/images/clove.png"
+};
+
+const projects = [
+  project1,
+  project2,
+  project3,
+  project4,
+  project5,
+  project6
+]
+
 function Portfolio() {
   return (
-    <div>
-      Portfolio
-    </div>
+    <main className="container">
+      <h1>
+        Portfolio
+      </h1>
+      <div class="block-descr block-descr-box project-display">
+        {/* {projects.map((project) => {
+
+        })} */}
+      </div>
+    </main>
   )
 }
 
