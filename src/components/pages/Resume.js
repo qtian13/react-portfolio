79a153c0f8de8eccf3b5eebf7a0e8e5abef4766b
qtian13 <qiushuang.tian@gmail.com>
2021-08-25 16:45:04 -0700

    styling for resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaRegFilePdf } from 'react-icons/fa';
+import '../../styles/Resume.css';
 
 const frontEndProficiencies = [
   'HTML',
@@ -26,25 +27,30 @@ const backEndProficiencies = [
 function Resume() {
   return (
     <main className="container mt-5">
-      <h1>
+      <h2>
         Resume
-      </h1>
-      <p>
+      </h2>
+      <h3 className="d-6 text-center mx-auto">
         View and download my <a target="_blank" href="./assets/files/QiushuangTianResume.pdf"><FaRegFilePdf /> resume</a>
-      </p>
-      <h2>Frontend Proficiencies</h2>
-      <ul>
-        {frontEndProficiencies.map((proficiency) => (
-          <li>{proficiency}</li>
-        ))}
-      </ul>
-      <h2>Backend Proficiencies</h2>
-      <ul>
-        {backEndProficiencies.map((proficiency) => (
-          <li>{proficiency}</li>
-        ))}
-      </ul>
-
+      </h3>
+      <div className="d-flex justify-content-around">
+        <div className="skills-section">
+          <h4>Frontend Proficiencies</h4>
+          <ul>
+            {frontEndProficiencies.map((proficiency) => (
+              <li>{proficiency}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="skills-section">
+          <h4>Backend Proficiencies</h4>
+          <ul>
+            {backEndProficiencies.map((proficiency) => (
+              <li>{proficiency}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </main>
   )
 }
