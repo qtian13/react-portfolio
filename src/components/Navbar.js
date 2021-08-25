2eb46d45ecaf881aa981d0543fdda656a4bcad37
qtian13 <qiushuang.tian@gmail.com>
2021-08-25 00:13:00 -0700

    deal with incorrect input

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Navbar({ currentPage, handlePageChange }) {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light px-5">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light px-5">
       <a className="navbar-brand" href="/">Qiushuang Tian</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,26 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { validateEmail, validateInput } from '../../utils/helpers';
 
 function Contact() {
+  const [viewerName, setViewerName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleOnChange = (e) => {
+    // Getting the name, email and message of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either email, username, and password
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'viewerName') {
+      setViewerName(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleOnBlur = (e) => {
+    // Getting the name, email and message of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+
+    // Based on the input type, we set the state of either email, username, and password
+    if (inputType === 'email' && !validateEmail(email)) {
+      setErrorMessage('Sorry, you entered an invalid email');
+    } else if (inputType === 'viewerName' && !validateInput(viewerName)) {
+      setErrorMessage('Sorry, a valid name is required');
+    } else if (inputType === 'message' && !validateInput(message)){
+      setErrorMessage('Sorry, a valid message is required');
+    } else {
+      setErrorMessage('');
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    // If everything goes according to plan, we want to clear out the input after a successful registration.
+    if (errorMessage === '') {
+      setViewerName('');
+      setEmail('');
+      setMessage('');
+    }
+  };
   return (
     <main className="container">
       <h1>
         Contact
       </h1>
       <form>
-      <div class="form-group my-2">
-          <label for="name" class="my-1">Name:</label>
-          <input type="text" class="form-control" id="name" placeholder="name" />
+      <div className="form-group my-2">
+          <label htmlFor="viewerName" className="my-1">Name:</label>
+          <input value={viewerName} name="viewerName" onBlur={handleOnBlur} onChange={handleOnChange} type="text" className="form-control" id="viewerName" placeholder="name" />
         </div>
-        <div class="form-group my-2">
-          <label for="email" class="my-1">Email address:</label>
-          <input type="email" class="form-control" id="email" placeholder="name@example.com" />
+        <div className="form-group my-2">
+          <label htmlFor="email" className="my-1">Email address:</label>
+          <input value={email} name="email" onBlur={handleOnBlur} onChange={handleOnChange} type="email" className="form-control" id="email" placeholder="name@example.com" />
         </div>
-        <div class="form-group my-2">
-          <label for="message" class="my-1">Message:</label>
-          <textarea class="form-control" id="message" rows="3"></textarea>
+        <div className="form-group my-2">
+          <label htmlFor="message" className="my-1">Message:</label>
+          <textarea value={message} name="message" onBlur={handleOnBlur} onChange={handleOnChange} className="form-control" id="message" rows="3"></textarea>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" onClick={handleFormSubmit} className="btn btn-primary">Submit</button>
       </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
     </main>
   )
 }
