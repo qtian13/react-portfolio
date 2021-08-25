6945c1c4b28208e8b1fc23bf7e35fd79ea6b0ffa
qtian13 <qiushuang.tian@gmail.com>
2021-08-25 00:29:15 -0700

    fill resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,10 +1,50 @@
 import React from 'react';
 
+const frontEndProficiencies = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'jQuery',
+  'responsive design',
+  'React',
+  'Bootstrap',
+  'Heroku'
+];
+
+const backEndProficiencies = [
+  'APIs',
+  'Node',
+  'Express',
+  'MySQL, Sequelize',
+  'MongoDB, Mongoose',
+  'REST',
+  'GraphQL'
+];
+
+
 function Resume() {
   return (
-    <div>
-      Resume
-    </div>
+    <main className="container">
+      <h1>
+        Resume
+      </h1>
+      <p>
+        Download my <a target="_blank" href="./files/QiushuangTianResume.pdf">resume</a>
+      </p>
+      <h2>Frontend Proficiencies</h2>
+      <ul>
+        {frontEndProficiencies.map((proficiency) => (
+          <li>{proficiency}</li>
+        ))}
+      </ul>
+      <h2>Backend Proficiencies</h2>
+      <ul>
+        {backEndProficiencies.map((proficiency) => (
+          <li>{proficiency}</li>
+        ))}
+      </ul>
+
+    </main>
   )
 }
 
