2c9af6e4a7ecb8ee9ca2012e8c53c7be7673a149
qtian13 <qiushuang.tian@gmail.com>
2021-08-25 16:00:11 -0700

    styling for AboutMe page

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -4,15 +4,15 @@ import '../../styles/AboutMe.css';
 function AboutMe() {
   return (
     <main className="container mt-5">
-      <h1>About Me</h1>
-      <img id="profile-photo" src="./assets/images/profile.jpg" alt="profile" />
-      <p>
+      <h2>About Me</h2>
+      <img id="profile-photo" className="d-block mx-auto" src="./assets/images/profile.jpg" alt="profile" />
+      <p className="mx-auto">
         Hi there! Welcome to my portfolio page! My name is Qiushuang Tian. I am a student of UC Berkeley Extention, Coding Boot Camp. I have so much fun working on different projects together with my team and do learn a lot on all kinds of web development skills! 
       </p>
-      <p>
-        This page presents several projects I recently finished. You can check out deployed page with a simple click! They are also pinned on my Github profile page. Please visit my Github page for more details.
+      <p className="mx-auto">
+        This page presents several projects I recently finished. You can check out deployed page and Github link with a simple click!
       </p>
-      <p>
+      <p className="mx-auto">
         If you consider me as a potential software developer candidate or have any advice, please do not hesitate to contact me! 
       </p>
     </main>
