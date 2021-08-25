b26507ca00432ff3c2c87620512b2148a982c597
qtian13 <qiushuang.tian@gmail.com>
2021-08-24 20:47:31 -0700

    edit footer component and install related package icons and bootstrap

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,36 @@
 import React from 'react';
-
+import { FaGithub, FaLinkedin, FaCopyright } from 'react-icons/fa'
 function FooterBlock() {
-  const text = 'footer';
+    let date = new Date();
+    let year = date.getFullYear();
+    return (
+<footer>
 
-  return <p>Hello World! Here is {text}</p>;
+  <div>
+
+    <div className="row">
+
+      <div className="col-md-12 py-5">
+        <div className="mb-5 flex-center">
+          <a className="mr-5 d-block" href="https://www.linkedin.com/in/qiushuang-tian-a9754248/">
+            <FaLinkedin />
+          </a>
+          <a className="ml-5 d-block" href="https://github.com/qtian13">
+            <FaGithub />
+          </a>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+
+  <div class="footer-copyright text-center">
+    Qiushuang Tian <FaCopyright /> 
+  </div>
+</footer>
+
+  );
 }
 
 export default FooterBlock;
