ea4cc9b5a69c7b668031155944b3a0eef9c3fbd1
qtian13 <qiushuang.tian@gmail.com>
2021-08-25 01:14:12 -0700

    add stackoverflow icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { FaGithub, FaLinkedin, FaCopyright } from 'react-icons/fa'
+import { FaGithub, FaLinkedin, FaStackOverflow, FaCopyright } from 'react-icons/fa'
 function FooterBlock() {
     return (
       <div className="d-block w-100" style={{backgroundColor: "lightgray",position: "absolute", bottom: "0px"}}>
         <footer>
           <div className="d-flex justify-content-center my-2">
-            <a className="d-inline-block mx-4 display-6" href="https://www.linkedin.com/in/qiushuang-tian-a9754248/">
+            <a className="d-inline-block mx-4 display-6" rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/qiushuang-tian-a9754248/">
               <FaLinkedin />
             </a>
-            <a className="d-inline-block mx-4 display-6" href="https://github.com/qtian13">
+            <a className="d-inline-block mx-4 display-6" rel="noreferrer" target="_blank" href="https://github.com/qtian13">
               <FaGithub />
             </a>
+            <a className="d-inline-block mx-4 display-6" rel="noreferrer" target="_blank" href="https://stackoverflow.com/users/16220053/qtian13?tab=profile">
+              <FaStackOverflow />
+            </a>
+            
           </div>
 
           <div className="text-center my-2">
