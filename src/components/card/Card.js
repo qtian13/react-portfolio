a9b83849051dfc935607586872722e6b1927919c
qtian13 <qiushuang.tian@gmail.com>
2021-08-25 18:35:30 -0700

    styling for project card

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { FaGithub, FaPlay } from 'react-icons/fa';
-const Card = ({ name, skills, deployed, github, demo }) => {
+import '../../styles/Card.css';
+
+const Card = ({ name, description, skills, deployed, github, demo }) => {
   return (
     <div
       className="project"
@@ -12,15 +14,18 @@ const Card = ({ name, skills, deployed, github, demo }) => {
         "width": "97%"
       }}
     >
-      <div className="project-content">
-        <h1 className="project-name">{name}</h1>
-        <a href={deployed} target="_blank" rel="noreferrer">
-          <FaPlay />
-        </a>
-        <a href={github} target="_blank" rel="noreferrer">
-          <FaGithub />
-        </a>
-        <p className="project-skills">{skills}</p>
+      <div className="project-content flex-column justify-content-center hide">
+        <h3 className="project-name text-center mb-1">{name}</h3>
+        <h3 className="project-description text-center mx-auto mb-0"><small>{description}</small></h3>
+        <div className="text-center my-3">
+          <a className="app-link" href={deployed} target="_blank" rel="noreferrer">
+            <FaPlay />
+          </a>
+          <a className="app-link d-inline-block mx-3" href={github} target="_blank" rel="noreferrer">
+            <FaGithub />
+          </a>
+        </div>
+        <p className="project-skills text-center mx-auto">{skills}</p>
       </div>
     </div>
   )
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -5,7 +5,8 @@ import Card from '../card/Card';
 const project1 = {
   id: "project1",
   name: "CLOVE",
-  skills: "CSS/JavaScript/NodeJS/Express/Handlebars/Sequelize/Cloudinary/Heroku",
+  skills: "NodeJS/Express/Sequelize/Heroku",
+  description: "Neighborhood grocery buy and sell",
   deployed: "https://still-reef-24172.herokuapp.com/",
   github: "https://github.com/qtian13/CLOVE",
   demo: "./assets/images/clove.png"
@@ -14,7 +15,8 @@ const project1 = {
 const project2 = {
   id: "project2",
   name: "Feed Your Boredom",
-  skills: "HTML/CSS/Bulma/JavaScript/jQuery/Openlayers",
+  skills: "HTML/CSS/Bulma/JavaScript/jQuery",
+  description: "Help user decide what to do and where to go",
   deployed: "https://qtian13.github.io/FeedYourBoredom/",
   github: "https://github.com/qtian13/FeedYourBoredom",
   demo: "./assets/images/feedYourBoredom.png"
@@ -23,7 +25,8 @@ const project2 = {
 const project3 = {
   id: "project3",
   name: "Workout Tracker",
-  skills: "CSS/JavaScript/NodeJS/MongoDB/Mongoose/Heroku",
+  skills: "CSS/JavaScript/NodeJS/MongoDB/Heroku",
+  description: "Record and analysis daily workout",
   deployed: "https://whispering-hamlet-60494.herokuapp.com/",
   github: "https://github.com/qtian13/workout-tracker",
   demo: "./assets/images/workoutTracker.png"
@@ -33,6 +36,7 @@ const project4 = {
   id: "project4",
   name: "Weather Dashboard",
   skills: "HTML/CSS/Bootstrap/JavaScript/jQuery",
+  description: "World weather at a glance",
   deployed: "https://qtian13.github.io/Weather-Dashboard/",
   github: "https://github.com/qtian13/Weather-Dashboard",
   demo: "./assets/images/weatherDashboard.png"
@@ -41,7 +45,8 @@ const project4 = {
 const project5 = {
   id: "project5",
   name: "Code Quiz",
-  skills: "CSS/JavaScript/NodeJS/Express/Handlebars/Sequelize/Cloudinary/Heroku",
+  skills: "HTML/CSS/JavaScript",
+  description: "Timed quiz to test coding knowledge",
   deployed: "https://still-reef-24172.herokuapp.com/",
   github: "https://github.com/qtian13/code-quiz",
   demo: "./assets/images/codeQuiz.png"
@@ -51,6 +56,7 @@ const project6 = {
   id: "project6",
   name: "Password Generator",
   skills: "HTML/CSS",
+  description: "Generate customized password",
   deployed: "https://qtian13.github.io/password-generator/",
   github: "https://github.com/qtian13/password-generator",
   demo: "./assets/images/passwordGenerator.png"
@@ -68,13 +74,13 @@ const projects = [
 function Portfolio() {
   return (
     <main className="container mt-5">
-      <h1>
+      <h2>
         Portfolio
-      </h1>
+      </h2>
       <div class="row block-descr block-descr-box project-display">
         {projects.map((project) => (
           <div className="col-12 col-md-6 my-3 px-2" style={{"height": "300px" }}>
-            <Card name={project.name} skills={project.skills} deployed={project.deployed} github={project.github} demo={project.demo}/>
+            <Card id={project.id} name={project.name} skills={project.skills} description={project.description} deployed={project.deployed} github={project.github} demo={project.demo}/>
           </div>
         ))}
       </div>
